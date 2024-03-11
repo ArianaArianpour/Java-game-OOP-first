949e0f8d90ce3ddd496d4bd3ce13ae0c4740f6b1
Ariana Arianpour <ariana.suboran@gmail.com>
2024-03-11 10:46:44 +0330

    Some changes on Player2

--- src/javaGame/Player2.java
+++ src/javaGame/Player2.java
@@ -1,5 +1,31 @@
 package javaGame;
 
-public class Player2 {
-
+public class Player2 extends Player1 {
+	
+	private int health;
+	private Boolean shield;
+	
+	public Player2(String name, String weapon, int health, boolean shield ) {
+		super(name, weapon, health);
+		this.health = health;
+		this.shield = shield;
+	}
+	
+	public void damageByGun1() {
+		if (shield) {
+			this.health -= 10;
+			if(this.health <= 0) {
+				this.health = 0;
+			}
+			System.out.println("Shield is ON! Got hit by Gun1. Health is Reduced by 10. New health is " + this.health);
+		}
+		if (!shield) {
+			this.health -= 40;
+			if(this.health <= 0) {
+				this.health = 0;
+			}
+			System.out.println("Shield is OFF! Got hit by Gun1. Health is Reduced by 40. New health is " + this.health);
+		}
+	}
 }
+
